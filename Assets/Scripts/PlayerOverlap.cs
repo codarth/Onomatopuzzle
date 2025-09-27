f79abdb706441d0f32348329355295d3d158c911
codarth <zimquo@gmail.com>
2025-09-27 14:28:00 -0600

    Removed need to add in references on Player

--- Assets/Scripts/PlayerOverlap.cs
+++ Assets/Scripts/PlayerOverlap.cs
@@ -6,57 +6,56 @@ using UnityEngine.Tilemaps;
 public class PlayerOverlap : MonoBehaviour
 {
     private Tilemap tilemap;
-    public WinningTile winningTile;
-    public DamageTile damageTile;
+    private WinningTile winningTile;
+    private DamageTile damageTile;
 
     private void Start()
     {
         tilemap = FindFirstObjectByType<Tilemap>();
-        if (tilemap != null)
-        {
-            Debug.Log("Tilemap found: " + tilemap.name);
-        }
-        else
-        {
-            Debug.LogWarning("No Tilemap found in the scene.");
-        }
+        if (tilemap == null) Debug.Log("tilemap not set in PlayerOverlap");
+        // get winningTile by path
+
+        winningTile = Resources.Load<WinningTile>("Tiles/WinningTile");
+        if (winningTile == null) Debug.Log("winningTile not found in PlayerOverlap");
+        damageTile = Resources.Load<DamageTile>("Tiles/DamageTile");
+        if (damageTile == null) Debug.Log("damageTile not found in PlayerOverlap");
     }
 
     private void Update()
-    {
-        Vector3Int playerCellPos = tilemap.WorldToCell(transform.position);
-        TileBase tile = tilemap.GetTile(playerCellPos);
+        {
+            Vector3Int playerCellPos = tilemap.WorldToCell(transform.position);
+            TileBase tile = tilemap.GetTile(playerCellPos);
 
-        if (tile == winningTile)
-        {
-            //Debug.Log("Player overlapped the Winning Tile!");
-            LoadNextLevel();
+            if (tile == winningTile)
+            {
+                //Debug.Log("Player overlapped the Winning Tile!");
+                LoadNextLevel();
+            }
+            else if (tile == damageTile)
+            {
+                // Debug.Log("Player overlapped the Damage Tile!");
+                DamagePlayer();
+            }
         }
-        else if (tile == damageTile)
-        {
-            // Debug.Log("Player overlapped the Damage Tile!");
-            DamagePlayer();
-        }
-    }
 
-    private void LoadNextLevel()
-    {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        int nextSceneIndex = currentSceneIndex + 1;
+        void LoadNextLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
 
-        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
-        {
-            SceneManager.LoadScene(nextSceneIndex);
+            if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                Debug.Log("No more levels to load. You reached the end of the game!");
+            }
         }
-        else
+
+        void DamagePlayer()
         {
-            Debug.Log("No more levels to load. You reached the end of the game!");
+            // Debug.Log("Player takes damage! Implement health reduction here.");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-    }
-    
-    private void DamagePlayer()
-    {
-        // Debug.Log("Player takes damage! Implement health reduction here.");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
 }
