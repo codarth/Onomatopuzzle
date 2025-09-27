0f9fd6b9bae0fe4a5df27249cd1185087aaee51c
codarth <zimquo@gmail.com>
2025-09-26 21:15:56 -0600

    Switching to git

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode)Key.Numpad1))
+        {
+            Debug.Log("Numpad 1 pressed");
+        }
+        else if (Input.GetKeyDown((KeyCode)Key.Numpad2))
+        {
+            
+        } else if
+    }
+}
